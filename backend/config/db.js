2b257edcedb50fac77ebe3f4dce8db61ffc39bc2
Ruchaka Ravisara <ruchakaravisara1@gmail.com>
2024-02-07 16:50:07 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,10 @@
+const {createPool} =require('mysql2');
+
+const pool = createPool({
+    port :3306,
+    host:"localhost",
+    user:"root",
+    password:"1234",
+    database : "mydb",
+    //connectionLimit :10
+})
