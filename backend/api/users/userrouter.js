abec31dfe90ccdf84241bfeb437d4fd2259bd6a2
Ruchaka Ravisara <ruchakaravisara1@gmail.com>
2024-02-07 22:38:03 +0530

    done

--- backend/api/users/userrouter.js
+++ backend/api/users/userrouter.js
@@ -1,11 +1,18 @@
-const {createUser,getUserByUserId,getUsers,updateUsers,deleteUser,login} =require("./usercontroller")
+const {
+  createUser,
+  getUserByUserId,
+  getUsers,
+  updateUsers,
+  deleteUser,
+  login,
+} = require("./usercontroller");
 const router = require("express").Router();
-const {checkToken} =require("../../auth/tokenValidation")
+const { checkToken } = require("../../auth/tokenValidation");
 
-router.post("/",checkToken,createUser);
-router.get("/",checkToken,getUsers);
-router.get("/:id",checkToken,getUserByUserId);
-router.patch("/",checkToken,updateUsers);
-router.delete("/",checkToken,deleteUser);
-router.post("/login",login)
+router.post("/", checkToken, createUser);
+router.get("/", checkToken, getUsers);
+router.get("/:id", checkToken, getUserByUserId);
+router.patch("/", checkToken, updateUsers);
+router.delete("/", checkToken, deleteUser);
+router.post("/login", login);
 module.exports = router;
