a99d8467d915818241520cb38a95f2ef058e4a2a
Ruchaka Ravisara <ruchakaravisara1@gmail.com>
2024-02-07 22:30:52 +0530

    authentication addded

--- backend/api/users/userrouter.js
+++ backend/api/users/userrouter.js
@@ -1,10 +1,11 @@
 const {createUser,getUserByUserId,getUsers,updateUsers,deleteUser,login} =require("./usercontroller")
 const router = require("express").Router();
+const {checkToken} =require("../../auth/tokenValidation")
 
-router.post("/",createUser);
-router.get("/",getUsers);
-router.get("/:id",getUserByUserId);
-router.patch("/",updateUsers);
-router.delete("/",deleteUser);
+router.post("/",checkToken,createUser);
+router.get("/",checkToken,getUsers);
+router.get("/:id",checkToken,getUserByUserId);
+router.patch("/",checkToken,updateUsers);
+router.delete("/",checkToken,deleteUser);
 router.post("/login",login)
 module.exports = router;
