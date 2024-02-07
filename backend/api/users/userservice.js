9f9900b7c96cea42c9e60d3937615c7b84de3392
Ruchaka Ravisara <ruchakaravisara1@gmail.com>
2024-02-07 19:20:51 +0530

    update userservice

--- backend/api/users/userservice.js
+++ backend/api/users/userservice.js
@@ -30,5 +30,18 @@ module.exports ={
             return callBack(null, results);
           }
         );
-      }
+      },
+      getUserByUserId: (id, callBack) => {
+        pool.query(
+          `select id,firstName,lastName,gender,email,number from registration where id = ?`,
+          [id],
+          (error, results, fields) => {
+            if (error) {
+              callBack(error);
+            }
+            return callBack(null, results[0]);
+          }
+        );
+      },
+      
 }
